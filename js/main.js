66b61e66e4132a912fcfc87b56cfa70f782fe8ac
edward karra <edwardkarra@gmail.com>
2021-10-30 23:07:13 +0200

    beatified

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 function welcome(){
+    let word = document.getElementById('hello_world');
+    setInterval(function(){
+        word.style.visibility = word.style.visibility === 'visible' ? 'hidden' : 'visible' ;
+    },400);
     let button = document.getElementById('click_me_button');
-    // button.hidden = true;
-
-        setInterval(function(){
-            let word = document.getElementById('hello_world');
-            word.style.visibility = word.style.visibility === 'visible' ? 'hidden' : 'visible' ;
-        },400);
-
-
+    button.hidden = true;
 }
